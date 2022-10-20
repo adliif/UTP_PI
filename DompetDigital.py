a3949b5b39ba2fc327467430faed66b7ae644867
ADLI FIQRULLAH <adli.fiqrullah21@students.unila.ac.id>
2022-10-20 19:24:25 +0700

    Update DompetDigital.py

--- DompetDigital.py
+++ DompetDigital.py
@@ -1,4 +1,5 @@
 import random
+import os
 listmenu = ["1. TOP UP GAME", "2. TAGIHAN RUMAH", "3. ISI PULSA"]
 listtopupgame = ["\n1. Mobile Legends", "2. PUBGM"]
 listtagihanrumah = ["\n1. PLN", "2. PDAM", "3. WIFI"]
@@ -9,6 +10,7 @@ def pengurangan(transaksi):
     total = print('Sisa uang Anda : Rp', saldo - transaksi)
     return total
 
+os.system('cls')
 print('SELAMAT DATANG DI APLIKASI DOMPET DIGITAL \n\n')
 passku = input('Silahkan masukkan password Anda untuk melanjutkan : ')
 
@@ -24,6 +26,7 @@ if passku == 'unila':
      
         if pilih == 1:
             try :
+                os.system('cls')
                 for i in range (0, len(listtopupgame)):
                     print(listtopupgame[i])
         
@@ -31,11 +34,13 @@ if passku == 'unila':
                 pilih1 != 1 or 2
 
                 if pilih1 == 1:
+                    os.system('cls')
                     pilihtopup = int(input("Silahkan masukan jumlah diamond yang ingin anda dapatkan : \n(WAJIB ANGKA!)\n\nJAWAB : "))
                     pilihtopup = pilihtopup*1000
                     print('\nTotal harga diamond yang anda keluarkan adalah : Rp.', pilihtopup)
                     pengurangan(pilihtopup)
                 elif pilih1 == 2:
+                    os.system('cls')
                     pilihtopup = int(input("Silahkan masukan jumlah diamond yg ingin anda dapatkan : \n(WAJIB ANGKA!)\n\nJAWAB : "))                 
                     pilihtopup = pilihtopup*2000
                     print('\nTotal harga diamond yang anda keluarkan adalah : Rp.', pilihtopup)
@@ -47,6 +52,7 @@ if passku == 'unila':
 
         elif pilih == 2:
             try :
+                os.system('cls)
                 for i in range (0, len(listtagihanrumah)):
                     print(listtagihanrumah[i])
         
@@ -54,14 +60,17 @@ if passku == 'unila':
                 pilih2 != 1 or 2 or 3
 
                 if pilih2 == 1:
+                    os.system('cls')
                     noPLN = int(input('Masukkan nomor pelanggan : '))
                     print('\nnomor token anda adalah : ',random.randint(1000,9999))
                     pengurangan(50000)
                 elif pilih2 == 2:
+                    os.system('cls')
                     noPDAM = int(input('Masukkan nomor pelanggan : '))
                     print('\nnomor token anda adalah : ',random.randint(1000,9999))
                     pengurangan(50000)
                 elif pilih2 == 3:
+                    os.system('cls')
                     noWIFI = int(input('Masukkan nomor pelanggan : '))
                     print('\nnomor token anda adalah : ',random.randint(1000,9999))
                     pengurangan(150000)
@@ -72,6 +81,7 @@ if passku == 'unila':
         
         elif pilih == 3:
             try :
+                os.system('cls')
                 for i in range (0, len(listisipulsa)):
                     print(listisipulsa[i])
         
@@ -79,6 +89,7 @@ if passku == 'unila':
                 pilih3 != 1 or 2
 
                 if pilih3 == 1:
+                    os.system('cls')
                     nomorHp = int(input('Masukkan nomor Hp\t\t: '))
                     nominal = int(input('Masukkan nominal isi pulsa\t: '))
                     if nominal < 10000:
@@ -86,6 +97,7 @@ if passku == 'unila':
                     else:
                         pengurangan(nominal)
                 elif pilih3 == 2:
+                    os.system('cls')
                     nomorHp = int(input('Masukkan nomor Hp\t\t: '))
                     nominal = int(input('Masukkan nominal isi pulsa\t: '))
                     if nominal < 5000:
