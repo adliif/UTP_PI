93517b906ff46cc1d9f183dd6cc5c0aa9ef258ae
MuhammadSatria <92299364+MuhammadSatria09@users.noreply.github.com>
2022-10-22 20:46:47 +0700

    Update DompetDigital.py

--- DompetDigital.py
+++ DompetDigital.py
@@ -18,7 +18,6 @@ passku = input('Silahkan masukkan password Anda untuk melanjutkan : ')
 if passku == 'unila':
     print('Password anda benar\n')
 
-    
     try :
         os.system('cls')
         print('SELAMAT DATANG DI APLIKASI DOMPET DIGITAL UNILA\n\n')
